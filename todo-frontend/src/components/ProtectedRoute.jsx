fa1d5cf48f623bfb95fd5795d268e3dd6e1708a6
biyalizabraham08 <biyalizabraham08@gmail.com>
2026-06-18 19:59:30 +0530

    Fix root redirect to point to landing page

--- todo-frontend/src/components/ProtectedRoute.jsx
+++ todo-frontend/src/components/ProtectedRoute.jsx
@@ -17,7 +17,7 @@ const ProtectedRoute = ({ children }) => {
   }
 
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/landing" replace />;
   }
 
   return children;
